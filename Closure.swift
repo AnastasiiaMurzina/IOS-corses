223539faeba6379cb7493c226ccad020ab75b830
AnastasiiaMurzina <hloshkin@mail.ru>
2017-06-13 19:29:15 +0400

    Create Closure.swift

---
+++ Closure.swift
@@ -0,0 +1,10 @@
+//task 1
+import Foundation
+
+var arr = ["Ann", "Antoni", "Sia", "Alex"]
+print(arr.sorted( by: { s1, s2 in String(s1.characters.reversed()) < String(s2.characters.reversed())}))
+
+//task 2
+let arr = ["12", "34", "-76", "0", "5"]
+let farray = arr.filter {!$0.contains("-")}
+print(farray.map{Int($0)!})

59c2ffcfa88e835220d90b11ace7af0dc57bd225
AnastasiiaMurzina <hloshkin@mail.ru>
2017-06-14 13:16:54 +0400

    Create Enum1.swift

--- Enum1.swift
+++ Enum1.swift
@@ -22,11 +22,11 @@ enum NetworkType {
 	case None
 	 func simpleDescription() -> String {
         switch self {
-        case .wifi(name: "name"):
+        case .wifi(name: _ ):
             return "wifi - name"
-		case .g3(distance: 0):
+		case .g3(distance: _ ):
 			return "3g - distance"
-		case .g4(distance: 0, code: 0):
+		case .g4(distance: _ , code: _ ):
 			return "4g - distance, code"
 		default:
 			return "none - none"
@@ -34,16 +34,21 @@ enum NetworkType {
 	 }
 	
 }
-let wf = NetworkType.wifi(name:"name")
+let wf = NetworkType.wifi(name: "hey")
 let wfDescription = wf.simpleDescription()
 print(wfDescription)
 
 //task3
-
 class Student{
-	let name = ""
-	let surname = ""
-	let average = 0.0
+	let name: String
+	let surname: String
+	let average: Double
+	
+	init(_ name: String, _ surname: String, _ average: Double){
+		self.name = name
+		self.surname = surname
+		self.average = average
+	}
 }
 
 class StudentGroup{
@@ -51,24 +56,30 @@ class StudentGroup{
 	
 	func avGroup() -> Double {
 		var counter = 0.0
-		var k = 0
 		for item in self.group {
-			k+=1
 			counter+=item.average			
 		}
-		return Double(counter)/Double(k)
+		return Double(counter)/Double(self.group.count)
 	}
 	
-	init(_ st: Student){
+	func app(_ st: Student) {
 		self.group.append(st)
 	}
-	func remove(object: Student) {
-        if let index = index(where: { $0 === object }) {
-             self.group.remove(at: index)
-        }
-    }
-	
-	
-	
+	func delete(_ st: Student) {
+    self.group = self.group.filter() { $0 !== st }
+	}
+	func getBetter(){
+		self.group.sort( by: { s1, s2 in s1.average > s2.average} )
+		print("The best students are \(self.group[0].surname), \(self.group[1].surname), \(self.group[2].surname)")
+	}	
 }
-  
+
+let stud = Student("Ann","N",1.0)
+let stud1 = Student("Anna","NN",3.0)
+let stud2 = Student("Nick","M",4.0)
+var stg = StudentGroup()
+stg.app(stud)
+stg.app(stud1)
+stg.app(stud2)
+print(stg.avGroup())
+stg.getBetter()

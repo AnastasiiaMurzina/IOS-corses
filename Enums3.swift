e97a2903e810910ec5b260b0c1d592b17720a443
AnastasiiaMurzina <hloshkin@mail.ru>
2017-06-25 14:24:02 +0400

    Update Enums3.swift

--- Enums3.swift
+++ Enums3.swift
@@ -33,3 +33,89 @@ class Person{
 	}
 	deinit{}
 }
+
+//task 2 - don't understand at all
+//task 3
+class Book {
+	var title = ""
+	var author = ""
+	var year = 0
+	init(title: String, author: String, year: Int) {
+		self.title = title
+		self.author = author
+		self.year = year
+	}
+}
+
+class Magazine: Book {
+	var article = ""
+	init(title: String, author: String, year: Int, article: String) {
+		super.init (title: title, author: author, year: year)
+		self.article = article
+	}
+}
+
+class FashionMagazine: Magazine {
+	var total = 0.0
+	init(title: String, author: String, year: Int, article: String, total: Double) {
+		super.init (title: title, author: author, year: year, article: article)
+		self.total = total
+	}
+}
+
+extension FashionMagazine {
+	func info() {
+		print(self.author, self.title, self.year)
+	}
+}
+extension Magazine {
+	func info() {
+		print(self.author, self.title, self.year)
+	}
+}
+extension Book {
+	func info() {
+		print(self.author, self.title, self.year)
+	}
+}
+//task 4 
+
+protocol Comparable{
+	func lessOrMore(first: Point, second: Point) -> String
+}
+
+struct Point: Comparable {
+	var x: Double
+	var y: Double
+	func lessOrMore(first: Point, second: Point) -> String {
+		if ((first.x > second.x) && (first.y > second.y)) {
+			return "More"
+		} else {
+			if ((first.x < second.x) && (first.y < second.y)) {
+			return "Less"
+			} 
+			return "I cann't"
+		}
+	}
+}
+
+//task 5
+
+class Point {
+	var x: Double
+	var y: Double
+	init (x: Double, y: Double) {
+		self.x = x
+		self.y = y
+	}
+}
+
+extension Point{
+	var average: Point{
+		get {
+			return Point(x: -self.x, y: -self.y)
+		}
+	}
+}
+
+
